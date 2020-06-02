29d5298f56e191257198657ddc1665c8312c49d5
shan <1096452045@qq.com>
2020-06-02 15:07:25 +0800

    初始化

---
+++ IJSPhotoSDK_Swift/IJSPhotoSDK_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IJSPhotoSDK_Swift
+//
+//  Created by 山神 on 2020/6/2.
+//  Copyright © 2020 山神. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IJSPhotoSDK_Swift/IJSPhotoSDK_SwiftTests/IJSPhotoSDK_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  IJSPhotoSDK_SwiftTests.swift
+//  IJSPhotoSDK_SwiftTests
+//
+//  Created by 山神 on 2020/6/2.
+//  Copyright © 2020 山神. All rights reserved.
+//
+
+import XCTest
+@testable import IJSPhotoSDK_Swift
+
+class IJSPhotoSDK_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
